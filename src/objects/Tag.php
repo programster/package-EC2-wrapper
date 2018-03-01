e441f0bc75538f0e34f997e850cc79c480b34a54
Programster <sdpagent@gmail.com>
2018-03-01 21:12:24 +0000

    Adding the Tag object

---
+++ src/objects/Tag.php
@@ -0,0 +1,34 @@
+<?php
+
+/* 
+ * A tag object that goes within a TagSpecification.
+ */
+
+namespace iRAP\Ec2Wrapper\Objects;
+
+class Tag
+{
+    private $m_key;
+    private $m_value;
+    
+    
+    /**
+     * Create a tag
+     * @param string $key - the name/key for the tag
+     * @param string $value - the value for the tag.
+     */
+    public function __construct(string $key, string $value)
+    {
+        $this->m_key = $key;
+        $this->m_value = $value;
+    }
+    
+    
+    public function toArray()
+    {
+        return array(
+            'Key' => $this->m_key,
+            'Value' => $this->m_value
+        );
+    }
+}
